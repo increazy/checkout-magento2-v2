dbaae244b6a632bd96744e549244f883d3f01689
Leonardo Vilarinho <vilarinholeo@outlook.com>
2023-04-12 12:31:03 -0300

    Update Finish.php

--- CheckoutV2/Controller/Payment/Finish.php
+++ CheckoutV2/Controller/Payment/Finish.php
@@ -76,6 +76,11 @@ class Finish extends Controller
             $order->setBaseTotalDue($order->getBaseTotalDue() + $body->tax);
         } else if ($body->tax > 0) {
             $order->setTaxAmount($order->getTaxAmount() + $body->tax);
+            
+            $order->setBaseGrandTotal($order->getBaseGrandTotal() + $body->tax);
+            $order->setTotalDue($order->getTotalDue() + $body->tax);
+            $order->setGrandTotal($order->getGrandTotal() + $body->tax);
+            $order->setBaseTotalDue($order->getBaseTotalDue() + $body->tax);
         }
         $order->save();
 
