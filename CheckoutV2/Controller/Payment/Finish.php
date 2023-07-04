0a83d16303d06983f964234132a5a33359764189
Leonardo Vilarinho <vilarinholeo@outlook.com>
2023-07-04 13:48:15 -0300

    Update Finish.php

--- CheckoutV2/Controller/Payment/Finish.php
+++ CheckoutV2/Controller/Payment/Finish.php
@@ -9,6 +9,8 @@ use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Sales\Model\Order;
 use Magento\Store\Model\StoreManagerInterface;
+use Magento\Sales\Model\Service\InvoiceService;
+use Magento\Framework\DB\Transaction;
 
 class Finish extends Controller
 {
@@ -24,17 +26,28 @@ class Finish extends Controller
      * @var CustomerRepositoryInterface
      */
     private $customer;
+    /**
+     * @var InvoiceSender
+     */
+    private $invoiceSender;
+    /**
+     * @var Transaction
+     */
+    private $transaction;
 
     public function __construct(
         Context $context,
         QuoteManagement $quoteManagement,
         CustomerRepositoryInterface $customer,
         Quote $quote,
+        InvoiceSender $invoiceSender,
+        Transaction $transaction,
         StoreManagerInterface $store,
         ScopeConfigInterface $scopeConfig
     )
     {
-        
+        $this->invoiceService = $invoiceService;
+        $this->transaction = $transaction;
         $this->quoteManagement = $quoteManagement;
         $this->quote = $quote;
         $this->customer = $customer;
@@ -117,6 +130,45 @@ class Finish extends Controller
                     $order->setState($state)->setStatus($state);
                 }
             }
+
+            if ($body->payment_data->status == 'success') {
+                if ($order->canUnhold()) {
+                    $order->unhold();
+                }
+
+                if ($order->canInvoice()) {
+                    $invoice = $this->invoiceService->prepareInvoice($order);
+                    $invoice->register();
+                    $invoice->save();
+
+                    $transactionSave = $this->transaction->addObject($invoice)
+                        ->addObject($invoice->getOrder());
+                    $transactionSave->save();
+
+                    $this->invoiceSender->send($invoice);
+
+                    $order
+                        ->addStatusHistoryComment('Pagamento confirmado')
+                    ->setIsCustomerNotified(true);
+
+                    $state = \Magento\Sales\Model\Order::STATE_PROCESSING;
+                    $order->setState($state)->setStatus($state);
+                }
+            }
+
+            if ($body->payment_data->status == 'canceled') {
+                if ($order->canUnhold()) {
+                    $order->unhold();
+                }
+
+                if ($order->canCancel()) {
+                    $order->cancel();
+                }
+                $state = \Magento\Sales\Model\Order::STATE_CANCELED;
+                $order->setState($state)->setStatus($state);
+            }
+
+            $order->save();
             
         } catch (\Exception $e) {
             $this->error($e->getMessage());
