935e95040cf365d8bad700d93acbf2f075ff71c8
Leonardo Vilarinho <vilarinholeo@outlook.com>
2023-07-06 17:15:00 -0300

    Update Process.php

--- CheckoutV2/Controller/Payment/Process.php
+++ CheckoutV2/Controller/Payment/Process.php
@@ -6,8 +6,6 @@ use Magento\Framework\App\Action\Context;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Sales\Model\Order;
 use Magento\Store\Model\StoreManagerInterface;
-use Magento\Sales\Model\Service\InvoiceService;
-use Magento\Framework\DB\Transaction;
 
 class Process extends Controller
 {
@@ -17,28 +15,21 @@ class Process extends Controller
     private $order;
 
     private $orderSender;
-    /**
-     * @var InvoiceSender
-     */
     private $invoiceSender;
-    /**
-     * @var Transaction
-     */
+
     private $transaction;
 
     public function __construct(
         Context $context,
         Order $order,
-        InvoiceSender $invoiceSender,
-        Transaction $transaction,
         StoreManagerInterface $store,
         ScopeConfigInterface $scopeConfig
     )
     {
-        $this->invoiceService = $invoiceService;
-        $this->transaction = $transaction;
-        $this->order = $order;
         $om = \Magento\Framework\App\ObjectManager::getInstance();
+        $this->invoiceService = $om->create(\Magento\Sales\Model\Service\InvoiceService::class);
+        $this->transaction = $om->create(\Magento\Framework\DB\Transaction::class);
+        $this->order = $order;
         $this->orderSender = $om->create(\Magento\Sales\Model\Order\Email\Sender\OrderSender::class);
         parent::__construct($context, $store, $scopeConfig);
     }
