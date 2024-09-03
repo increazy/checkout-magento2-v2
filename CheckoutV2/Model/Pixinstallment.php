b3f44eeee7e98a8d98abc935135570a2f556f987
Leonardo Vilarinho <leonardo@increazy.com>
2024-09-03 15:09:55 -0300

    adiciona pix parcelado e voucher

---
+++ CheckoutV2/Model/Pixinstallment.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Increazy\CheckoutV2\Model;
+
+class Pixinstallment extends AbstractMethod
+{
+    protected $_code = 'increazy-pix-installment';
+}
---
+++ CheckoutV2/Model/Voucher.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Increazy\CheckoutV2\Model;
+
+class Voucher extends AbstractMethod
+{
+    protected $_code = 'increazy-voucher';
+}
